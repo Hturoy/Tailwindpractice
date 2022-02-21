c2e2579cd70edae31090a57fea57b6ebaa1eb6b0
Hturoy <69842773+Hturoy@users.noreply.github.com>
2022-02-21 14:11:33 +0100

    component: added a Navbar Component(Not completed)

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaBeer } from "react-icons/fa";
+import { BiBong, BiBoltCircle } from "react-icons/bi";
+
+export const Navbar = () => {
+  return (
+    <nav className="flex row w-full bg-slate-500 h-24 justify-between">
+      <div className="flex items-center">
+        <a className="m-5 font-medium hover:font-black">Home</a>
+        <a className="m-5 font-medium hover:font-black">About</a>
+        <a className="m-5 font-medium hover:font-black">Contact</a>
+      </div>
+
+      <div className="bg-slate-400 flex items-center">
+        <FaBeer className=""></FaBeer>
+        <BiBong className=""></BiBong>
+        <BiBoltCircle></BiBoltCircle>
+      </div>
+    </nav>
+  );
+};
